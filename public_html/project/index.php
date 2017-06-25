33110322590c415612ccd593df603027be9eba0d
daamian3 <damianchojnacki@op.pl>
2017-06-25 18:57:19 +0200

    no fajnie

---
+++ public_html/project/index.php
@@ -0,0 +1,73 @@
+﻿<!DOCTYPE html>
+<html lang="pl">
+	<head>
+
+		<meta charset="UTF-8">
+		<title>Angielski dla bąka</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Spectral" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script src="parallax-background.min.js"></script>
+		<?php include 'detect.php'; ?>
+
+	</head>
+	<body>
+
+			<header id="top" class="main-header">
+						<ul class="main-nav">
+							<a href="index.php"><li class="logo"><h1>Angielski dla bąka</h1></li></a>
+							<div class="buttons">
+								<a href="logowanie.php"><li class="button">Zaloguj się</li></a>
+							<a href="rejestracja.php"><li class="button">Zarejestruj się</li></a>
+						</div>
+						</ul>
+				<div class="container">
+					<article>
+						<h2>O nas</h2>
+						<p>
+							Jesteśmy tutaj, aby sprawić żeby wasze dziecko nauczyło się języka angielskiego.
+							Ta niezwykle przydatna umiejętność jest bardzo ważna w dzisiejszych czasach,
+							dlatego warto rozpocząć naukę jak najprędzej. Oferujemy naukę języka, poprzez
+							przyjazne i zachęcające gry dzięki którym wasze maluchy nie będą myśleć o 	tym, że się uczą. Suspendisse tempor venenatis pharetra. Quisque massa quam, facilisis convallis faucibus nec, auctor a est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+						</p>
+					</article>
+				</div>
+			</header>
+
+			<div class="container">
+				<article class="graj">
+					<h2>GRAJ TERAZ!</h2>
+				</article>
+
+					<article>
+						<h2>Czemu akurat Angielski dla bąka</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum semper purus, at porta ipsum porta id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc maximus molestie convallis. Fusce nec odio dapibus, convallis dolor a, euismod eros. Mauris quis suscipit mi. Praesent lacus magna, pellentesque vel volutpat vel, pretium id nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+							Donec malesuada neque non mattis commodo. Maecenas laoreet posuere purus, sed ullamcorper erat molestie vel. Proin venenatis massa ut metus feugiat, at feugiat mi sodales. Mauris vulputate metus interdum leo volutpat, quis imperdiet dui tempor. Praesent tincidunt ligula metus, in sagittis velit viverra vitae. Aliquam ac odio id diam porta aliquet. Suspendisse tempor venenatis pharetra. Quisque massa quam, facilisis convallis faucibus nec, auctor a est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam at turpis interdum, scelerisque mi iaculis, varius justo. Nam nec turpis vestibulum, 	ullamcorper elit et, commodo augue.
+						</p>
+					</article>
+
+					<article class="english" data-parallax-bg-image="english.png" data-parallax-speed="1" data-parallax-direction="down"></article>
+
+					<article>
+						<h2>Efekty nauki widoczne po tygodniu</h2>
+						Vivamus eget nunc at risus suscipit dictum sit amet egestas velit. Aliquam sagittis neque augue, vitae scelerisque massa gravida id. Maecenas laoreet posuere purus, sed ullamcorper erat molestie vel. Proin venenatis massa ut metus feugiat, at feugiat mi sodales. Mauris vulputate metus interdum leo volutpat, quis imperdiet dui tempor. Praesent tincidunt ligula metus, in sagittis velit viverra vitae. Aliquam ac odio id diam porta aliquet. Suspendisse tempor venenatis pharetra. Praesent nec mauris a lorem hendrerit suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vulputate eros justo, sit amet cursus elit consequat ut. Praesent et lectus in dolor interdum finibus id eu enim. Maecenas faucibus arcu ut vestibulum vehicula. Sed euismod sapien convallis dolor rutrum ornare.
+					</article>
+
+				</div>
+
+
+			<footer class="main-footer">
+				<p>&copy;2017 | Nauka angielskiego - Angielski dla bąka&reg;</p>
+			</footer>
+
+			<script>
+				(function ($) {
+					$('.english').parallaxBackground();
+				})(jQuery);
+			</script>
+
+	</body>
+</html>
