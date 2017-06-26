ace7cb64207d8b3042aef3d7e7a5140af1046e36
daamian3 <damianchojnacki@op.pl>
2017-06-26 23:33:40 +0200

    czemu akurat, trzy ikony, ładowanie, poprawka graj teraz, mniej tekstu

--- public_html/project/index.php
+++ public_html/project/index.php
@@ -11,25 +11,39 @@
 		<script src="parallax-background.min.js"></script>
 		<?php include 'detect.php'; ?>
 
+
+
 	</head>
 	<body>
+		<!-- ładowanie się strony -->
+		<div id="przyciemnienie" class="przyciemnienie"></div>
+		<div id="loading" class="loading">
+			Trwa ładowanie strony...
+			<div class="loader"></div>
+		</div>
 
-			<header id="top" class="main-header">
-						<ul class="main-nav">
-							<a href="index.php"><li class="logo"><h1>Angielski dla bąka</h1></li></a>
-							<div class="buttons">
-								<a href="logowanie.php"><li class="button">Zaloguj się</li></a>
+		<!-- tu można wyłączyc -->
+		<script>
+		//document.getElementById("przyciemnienie").style.display="block";
+		//document.getElementById("loading").style.display="block";
+		</script>
+
+		<header id="top" class="main-header">
+					<ul class="main-nav">
+						<a href="index.php"><li class="logo"><h1>Angielski dla bąka</h1></li></a>
+						<div class="buttons">
+							<a href="logowanie.php"><li class="button">Zaloguj się</li></a>
 							<a href="rejestracja.php"><li class="button">Zarejestruj się</li></a>
 						</div>
-						</ul>
-				<div class="container">
-					<article>
-						<h2>O nas</h2>
-						<p>
-							Jesteśmy tutaj, aby sprawić żeby wasze dziecko nauczyło się języka angielskiego.
-							Ta niezwykle przydatna umiejętność jest bardzo ważna w dzisiejszych czasach,
-							dlatego warto rozpocząć naukę jak najprędzej. Oferujemy naukę języka, poprzez
-							przyjazne i zachęcające gry dzięki którym wasze maluchy nie będą myśleć o 	tym, że się uczą. Suspendisse tempor venenatis pharetra. Quisque massa quam, facilisis convallis faucibus nec, auctor a est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+					</ul>
+			<div class="container">
+				<article class="o_nas">
+					<h2>O nas</h2>
+					<p>
+						Jesteśmy tutaj, aby sprawić żeby wasze dziecko nauczyło się języka angielskiego.
+						Ta niezwykle przydatna umiejętność jest bardzo ważna w dzisiejszych czasach,
+						dlatego warto rozpocząć naukę jak najszybciej. Oferujemy naukę języka, poprzez
+						przyjazne i zachęcające gry dzięki którym wasze maluchy nie będą myśleć o tym, że się uczą.
 						</p>
 					</article>
 				</div>
@@ -37,19 +51,32 @@
 
 			<div class="container">
 				<article class="graj">
-					<h2>GRAJ TERAZ!</h2>
+					<h2>Wypróbuj już teraz!</h2>
 				</article>
 
 					<article>
 						<h2>Czemu akurat Angielski dla bąka</h2>
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum semper purus, at porta ipsum porta id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc maximus molestie convallis. Fusce nec odio dapibus, convallis dolor a, euismod eros. Mauris quis suscipit mi. Praesent lacus magna, pellentesque vel volutpat vel, pretium id nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-							Donec malesuada neque non mattis commodo. Maecenas laoreet posuere purus, sed ullamcorper erat molestie vel. Proin venenatis massa ut metus feugiat, at feugiat mi sodales. Mauris vulputate metus interdum leo volutpat, quis imperdiet dui tempor. Praesent tincidunt ligula metus, in sagittis velit viverra vitae. Aliquam ac odio id diam porta aliquet. Suspendisse tempor venenatis pharetra. Quisque massa quam, facilisis convallis faucibus nec, auctor a est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam at turpis interdum, scelerisque mi iaculis, varius justo. Nam nec turpis vestibulum, 	ullamcorper elit et, commodo augue.
+							Chwile poświęcone nauce na nauce z naszymi grami, zwrórcą się z pewnością w codziennych czynnościach, w których potrzebna jest znajomość języka angielskiego. Nasze gry:
 						</p>
+						<div class="czemu">
+							<div class="icon">
+								<img src="idea.svg" alt="inteligencja"><br />
+								Rozwijają umysł
+							</div>
+							<div class="icon">
+								<img src="chat.svg" alt="rozmowa"><br />
+								Uczą rozmawiać
+							</div>
+							<div class="icon">
+								<img src="clipboard.svg" alt="egzaminy"><br />
+								Pomagają w szkole
+							</div>
+						</div>
+
 					</article>
 
-					<article class="english" data-parallax-bg-image="english.png" data-parallax-speed="2" data-parallax-direction="down"></article>
+					<article class="english" data-parallax-bg-image="english.png" data-parallax-speed="1" data-parallax-direction="down"></article>
 
 					<article>
 						<h2>Efekty nauki widoczne po tygodniu</h2>
@@ -64,9 +91,19 @@
 			</footer>
 
 			<script>
-				(function ($) {
-					$('.english').parallaxBackground();
-				})(jQuery);
+
+			$(document).ready(function(){
+			  // Po poprawnym załadowaniu strony wyłącza przyciemnienie
+			  $("#przyciemnienie").fadeOut('fast');
+			  // Po poprawnym załadowaniu strony wyłącza loading
+			  $("#loading").fadeOut('fast');
+			});
+
+			(function ($) {
+				$('.english').parallaxBackground();
+			})(jQuery);
+
+
 			</script>
 
 	</body>
